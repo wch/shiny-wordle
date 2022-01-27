4dcd67b7bb52b5c5796e22cabf8a6f8c35acdd99
Winston Chang <winston@stdout.org>
2022-01-27 14:22:00 -0600

    UI improvements

--- app.R
+++ app.R
@@ -4,13 +4,21 @@ library(htmltools)
 source("wordlist.R")
 
 ui <- fluidPage(
+  theme = bslib::bs_theme(version = 4),
   title = "Shiny wordle",
   tags$style(HTML("
   .container-fluid {
       text-align: center;
+      height: calc(100vh - 30px);
+      display: grid;
+      grid-template-rows: 1fr auto;
   }
   .guesses {
-      margin: 10px 2px;
+      overflow-y: auto;
+      height: 100%;
+  }
+  .guesses.finished {
+      overflow-y: visible;
   }
   .guesses .word {
       margin: 5px;
@@ -45,9 +53,11 @@ ui <- fluidPage(
       border: 1px solid black;
   }
   .keyboard {
+      height: 240px;
+      user-select: none;
   }
   .keyboard .keyboard-row {
-    margin: 3px;
+      margin: 3px;
   }
   .keyboard .keyboard-row .key {
       display: inline-block;
@@ -61,14 +71,17 @@ ui <- fluidPage(
       font-size: 18px;
       font-weight: bold;
       vertical-align: middle;
-      user-select: none;
       color: black;
       font-family: 'Clear Sans', 'Helvetica Neue', Arial, sans-serif;
       background-color: #ddd;
+      touch-action: none;
+  }
+  .keyboard .keyboard-row .key:focus {
+      outline: none;
   }
   .keyboard .keyboard-row .key.wide-key {
-    font-size: 15px;
-    width: 60px;
+      font-size: 15px;
+      width: 50px;
   }
   .keyboard .keyboard-row .key.correct {
       background-color: #6a5;
@@ -92,27 +105,27 @@ ui <- fluidPage(
       padding: 20px;
       border-radius: 5px;
       box-shadow: 4px 4px 19px rgb(0 0 0 / 17%);
-  }")),
-  h3("Shiny wordle"),
+  }
+")),
   div(
     class = "guesses",
+    h3("Shiny wordle"),
     uiOutput("previous_guesses"),
-    uiOutput("current_guess")
+    uiOutput("current_guess"),
+    uiOutput("endgame"),
+    uiOutput("new_game_ui")
   ),
-  uiOutput("endgame"),
   uiOutput("keyboard"),
-  uiOutput("new_game_ui"),
-  div(
-    style="display: inline-block;",
-    checkboxInput("hard", "Hard mode")
-  ),
+  # div(
+  #   style="display: inline-block;",
+  #   checkboxInput("hard", "Hard mode")
+  # ),
   tags$script(HTML("
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const all_key_ids = [ ...letters, 'Enter', 'Back'];
     document.addEventListener('keydown', function(e) {
-      let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-                     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                     'Y', 'Z'];
       let key = e.code.replace(/^Key/, '');
-      console.log(key);
       if (letters.includes(key)) {
         document.getElementById(key).click();
       } else if (key == 'Enter') {
@@ -120,7 +133,35 @@ ui <- fluidPage(
       } else if (key == 'Backspace') {
         document.getElementById('Back').click();
       }
+    });
 
+    // For better responsiveness on touch devices, trigger a click on the button
+    // when a touchstart event occurs; don't wait for the touchend event. So
+    // that a click event doesn't happen when the touchend event happens (and
+    // cause the letter to be typed a second time), we set the 'pointer-events'
+    // CSS property to 'none' on the button. Then when there's _any_ touchend
+    // event, unset the 'pointer-events' CSS property on all of the buttons, so
+    // that the button can be touched again.
+    let in_button_touch = false;
+    document.addEventListener('touchstart', function(e) {
+        if (all_key_ids.includes(e.target.id)) {
+            e.target.click();
+            e.target.style.pointerEvents = 'none';
+            e.preventDefault();   // Disable text selection
+            in_button_touch = true;
+        }
+    });
+    document.addEventListener('touchend', function(e) {
+        all_key_ids.map((id) => {
+            document.getElementById(id).style.pointerEvents = null;
+        });
+        if (in_button_touch) {
+            if (all_key_ids.includes(e.target.id)) {
+                // Disable text selection and triggering of click event.
+                e.preventDefault();
+            }
+            in_button_touch = false;
+        }
     });
   "))
 )
@@ -145,13 +186,13 @@ server <- function(input, output) {
     if (! guess %in% words_all)
       return()
 
-    if (input$hard) {
-      # # Letters in the target word that the player has previously
-      # # guessed correctly.
-      # matched_letters = used_letters().intersection(set(target_word()))
-      # if not set(guess).issuperset(matched_letters):
-      #     return
-    }
+    # if (input$hard) {
+    # # Letters in the target word that the player has previously
+    # # guessed correctly.
+    # matched_letters = used_letters().intersection(set(target_word()))
+    # if not set(guess).issuperset(matched_letters):
+    #     return
+    # }
 
     all_guesses_new <- all_guesses()
 
@@ -184,7 +225,11 @@ server <- function(input, output) {
       div(class = "word", row)
     })
 
-    tagList(res)
+    scroll_js <- "
+        document.querySelector('.guesses')
+          .scrollTo(0, document.querySelector('.guesses').scrollHeight);
+    "
+    tagList(res, tags$script(HTML(scroll_js)))
   })
 
   output$current_guess <- renderUI({
@@ -280,6 +325,8 @@ server <- function(input, output) {
   lapply(unlist(keys, recursive = FALSE), function(key) {
     if (key %in% c("Enter", "Back")) return()
     observeEvent(input[[key]], {
+      if (finished())
+        return()
       cur <- current_guess_letters()
       if (length(cur) >= 5)
         return()
